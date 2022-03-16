1a8573fc910324ae8e8018197693384eba66e512
Volo Pivoshenko <40499728+volodymyrpivoshenko@users.noreply.github.com>
2022-03-16 22:09:43 +0000

    fix: implement warning decorator using pure `Python`

---
+++ src/configflow/misc/decorators.py
@@ -0,0 +1,32 @@
+"""Module for the decorators."""
+
+from __future__ import annotations
+
+import warnings
+
+from typing import Any
+from typing import Callable
+
+
+def deprecated(function: Callable[[Any], Any]) -> Any:
+    """Show warning about the depreciation of a function or method.
+
+    Examples
+    --------
+    >>> @deprecated
+    >>> def logger(handler: str, filters: list) -> None:
+    ...     ...
+    >>> with warnings.catch_warnings(record=True) as warning:
+    ...     warnings.simplefilter("always")
+    ...     logger(..., ...)
+    ...     warning
+    [<warnings.WarningMessage object at ...]
+    """
+
+    def wrapper(*args, **kwargs) -> Any:
+        """Show warning about the depreciation of a function or method."""
+
+        warnings.warn("Function {0!r} is deprecated.".format(function.__name__), DeprecationWarning)
+        return function(*args, **kwargs)
+
+    return wrapper
