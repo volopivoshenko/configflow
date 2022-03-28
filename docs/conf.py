04054cbff8c6160cbc3f8a8f5451bb7577f6fe00
Volodymyr Pivoshenko <40499728+volodymyrpivoshenko@users.noreply.github.com>
2022-03-28 22:25:06 +0100

    docs: update theme

--- docs/conf.py
+++ docs/conf.py
@@ -25,14 +25,13 @@ release = "0.1.2"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.napoleon",
-    "sphinx.ext.coverage",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.autosectionlabel",
-    "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
-    "autodocsumm",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 autosummary_generate = True
@@ -59,12 +58,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_book_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ["_static"]
-
 html_favicon = "_static/assets/logo.ico"
